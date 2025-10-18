4acaa8ca3b7a14e2391b442abb33a2700a9ce29c
Vimanyu Jain <vimanyujain07@gmail.com>
2025-10-18 23:25:00 +0530

    changed main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:get/get_navigation/src/routes/get_route.dart';
-
-import 'feature/Get Started/screens/get_started.dart';
-import 'feature/SignupLogin/screen/otp_verification.dart';
-import 'feature/SignupLogin/screen/signup_login.dart';
-import 'feature/referal/screen/referal_signup.dart';
 import 'feature/welcome screen/screen/welcome_screen.dart';
 
 void main() {
